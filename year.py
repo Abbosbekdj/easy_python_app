7f1dfb82d7d310a5fdd4b30efd7898835276ce92
Abbosbek Abdujabborov <137617450+Abbosbekdj@users.noreply.github.com>
2025-05-20 09:43:12 +0500

    Delete year.py

--- year.py
+++
@@ -1,8 +0,0 @@
-# 10/05/2025
-"""
-5. Yil kabisa yilmi?
-Foydalanuvchi yil kiritadi, agar u 4 ga qoldiqsiz bo‘linsa va 100 ga bo‘linmasa yoki 400 ga bo‘linsa, "Kabisa yili", 
-aks holda "Oddiy yil" deb chiqaring.
-"""
-
-print("Yil kiriting men uni Kabisa yilimi yoki yo'qmi shuni aniqlab beraman :)")
